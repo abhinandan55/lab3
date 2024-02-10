1a49f7a2969be7f67f66802514efac483c7bdbfa
abhinandan55 <159602112+abhinandan55@users.noreply.github.com>
2024-02-10 15:23:15 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,8 @@ public class Main {
   public static void main(String[] args) {
 
     int num = 29;
+    System.out.println("Program 1");
+
     boolean flag = false;
     for (int i = 2; i <= num / 2; ++i) {
       if (num % i == 0) {
