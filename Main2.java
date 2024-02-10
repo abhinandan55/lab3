a5b9c7f637a040802bba28a400b9ff41f85d21de
abhinandan55 <159602112+abhinandan55@users.noreply.github.com>
2024-02-10 15:23:48 +0530

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -3,6 +3,8 @@ public  class Main2
   public  static void main(String[] args)
   {
     int n =5;
+    System.out.println("Program 2");
+
     for (int i=1 ; i<=n ; i++)
       System.out.println(i);
   }
